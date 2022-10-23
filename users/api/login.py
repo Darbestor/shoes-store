fba6f8c7f6f2eb2e2c05b1191f36190f4912d17a
darbestor <fordarbe@gmail.com>
2022-10-23 20:01:38 +0300

    implemented login post endpoint

--- users/api/login.py
+++ users/api/login.py
@@ -1,6 +1,6 @@
 """Login router"""
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 
 from models.requests import LoginReq
@@ -8,9 +8,22 @@ from repository.login import LoginRepository
 
 router = APIRouter(prefix="/login", tags=["login"])
 
-# @router.post("/")
-# async def register_user(req: LoginReq, repo: LoginRepository = Depends()):
-#     try:
-#         details = req.dict(exclude_unset=True)
-#         if await repo.add_login(details) is True:
-#             return
+
+@router.post("/", status_code=201)
+async def register_user(req: LoginReq, repo: LoginRepository = Depends()):
+    """Add new user
+
+    Args:
+        req (LoginReq): request payload
+        repo (LoginRepository, optional): repository.
+
+    Returns:
+        LoginReq: Login response with user id
+    """
+
+    details = req.dict(exclude_unset=True)
+    login_id = await repo.add_login(details)
+    if login_id is not None:
+        req.id = login_id
+        return req
+    raise HTTPException(status_code=500, detail="Fail to register user")
