68719986613c3f5d963c89b8876d22d0976cadf2
darbestor <fordarbe@gmail.com>
2022-10-23 19:13:52 +0300

    added login repostiry adn added insert method

---
+++ users/repository/login.py
@@ -0,0 +1,36 @@
+"""Repository for interaction with login table in database"""
+
+from fastapi import Depends
+from sqlalchemy.orm import sessionmaker
+
+from config.db import get_db_connection
+from models.db import Login
+
+
+class LoginRepository:
+    """Access to data in login table"""
+
+    def __init__(
+        self, session_factory: sessionmaker = Depends(get_db_connection)
+    ) -> None:
+        self.session_factory: sessionmaker = session_factory
+
+    async def add_login(self, details: dict) -> bool:
+        """Insert new Login entry
+
+        Args:
+            details (dict): parameters
+
+        Returns:
+            bool: transaction status. True if inserted, False on error
+        """
+
+        try:
+            async with self.session_factory() as session:  # type: ignore
+                async with session.begin():
+                    login = Login(**details)
+                    await session.add(login)
+        except Exception as ex:
+            print(ex)
+            return False
+        return True
