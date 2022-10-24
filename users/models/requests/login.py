36c7d3c8aa8e5a9e3dcf146538e89d9cbf68b3fd
darbestor <fordarbe@gmail.com>
2022-10-24 13:45:59 +0300

    added response model and changed request model

--- users/models/requests/login.py
+++ users/models/requests/login.py
@@ -1,12 +1,10 @@
 """Login request models"""
 
-from uuid import UUID
 from pydantic import BaseModel
 
 
 class LoginReq(BaseModel):
     """Login request"""
 
-    id: UUID | None = None
     username: str
     password: str
