7e2aa4bc94b6b73b1976f3d4d1266759492e7047
darbestor <fordarbe@gmail.com>
2022-10-24 13:46:08 +0300

    added get endpoints

--- users/api/login.py
+++ users/api/login.py
@@ -1,15 +1,18 @@
 """Login router"""
 
-from fastapi import APIRouter, Depends, HTTPException
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.responses import Response
 
 
 from models.requests import LoginReq
+from models.responses.login import LoginResponse
 from repository.login import LoginRepository
 
 router = APIRouter(prefix="/login", tags=["login"])
 
 
-@router.post("/", status_code=201)
+@router.post("/", status_code=201, response_model=LoginResponse)
 async def register_user(req: LoginReq, repo: LoginRepository = Depends()):
     """Add new user
 
@@ -24,6 +27,35 @@ async def register_user(req: LoginReq, repo: LoginRepository = Depends()):
     details = req.dict(exclude_unset=True)
     login_id = await repo.add_login(details)
     if login_id is not None:
-        req.id = login_id
-        return req
+        response = LoginResponse(id=login_id, username=req.username)
+        return response
     raise HTTPException(status_code=500, detail="Fail to register user")
+
+
+@router.get("/", response_model=list[LoginResponse])
+async def get_users(repo: LoginRepository = Depends()):
+    """Retrieve all logins
+
+    Args:
+        req (LoginReq): request payload
+        repo (LoginRepository, optional): repository.
+    """
+
+    logins = await repo.get_logins()
+    response = [LoginResponse(id=l.id, username=l.username) for l in logins]
+    return response
+
+
+@router.get("/{login_id}", response_model=LoginResponse)
+async def get_user_by_id(login_id: UUID, repo: LoginRepository = Depends()):
+    """Get login info by id
+
+    Args:
+        login_id (UUID): user identifier
+        repo (LoginRepository, optional): repository
+    """
+
+    login = await repo.get_login_by_id(login_id)
+    if login is not None:
+        return LoginResponse(id=login.id, username=login.username)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
