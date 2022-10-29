7cb04444812c04f02752a8ba9bfa637221b272ad
darbestor <fordarbe@gmail.com>
2022-10-29 22:05:57 +0300

    added api for warehouse

---
+++ catalog/api/warehouse.py
@@ -0,0 +1,17 @@
+from uuid import UUID
+from fastapi import APIRouter, Depends
+
+from models.requests.warehouse import WarehouseReq
+from service.warehouse import WarehouseService
+
+
+router = APIRouter(prefix="/warehouse")
+
+
+@router.put(
+    "/{model_id}",
+)
+async def update_storage(
+    model_id: UUID, payload: WarehouseReq, service: WarehouseService = Depends()
+):
+    return await service.update_storage(model_id, payload)
--- catalog/main.py
+++ catalog/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 from config.db import init_db
 from api import model
+from api import warehouse
 
 
 app = FastAPI()
 app.include_router(model.router)
+app.include_router(warehouse.router)
 
 
 @app.on_event("startup")
