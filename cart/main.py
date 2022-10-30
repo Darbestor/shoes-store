2de37417966d7bf67a7613c845b2954016a5a309
darbestor <fordarbe@gmail.com>
2022-10-31 00:04:15 +0300

    exception handlers

--- cart/main.py
+++ cart/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 from config.db import init_db
+from exceptions import DBException
 
 
 app = FastAPI()
@@ -11,3 +13,8 @@ async def initialize():
     """Startup event"""
 
     await init_db()
+
+
+@app.exception_handler(DBException)
+async def db_exception_handler(_: Request, exc: DBException):
+    return JSONResponse(status_code=400, content={"database": exc.args[0]})
