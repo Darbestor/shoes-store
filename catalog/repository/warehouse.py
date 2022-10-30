1b8eb7c1d025082537b55eeee5abf617ccbec58d
darbestor <fordarbe@gmail.com>
2022-10-30 21:11:26 +0300

    refactor of change method

--- catalog/repository/warehouse.py
+++ catalog/repository/warehouse.py
@@ -34,44 +34,22 @@ class WarehouseRepository:
         await warehouse.save()
         return warehouse
 
-    async def return_pairs(self, model_id: UUID, size: int, quantity):
+    async def change_storage(self, model_id: UUID, size: int, quantity: int):
         """Return pairs back to storage
 
         Args:
             model_id (UUID): model_identifier
             size (int): model size
-            quantity (_type_): number of pairs to return
+            quantity (int): number of pairs.
+                Positive number - return models to storage
+                Negative number - take models from storage.
 
-        """
-        warehouse, storage = await self.__get_storage(model_id, size)
-        storage.quantity += quantity
-        await warehouse.save()
-
-    async def take_pairs(self, model_id: UUID, size: int, quantity):
-        """Take models from warehouse
-
-        Args:
-            model_id (UUID): model_identifier
-            size (int): model size
-            quantity (_type_): number of pairs to take
-
-        """
-        warehouse, storage = await self.__get_storage(model_id, size)
-        if quantity > storage.quantity:
-            raise ValueException(f"Not enough pairs in warehouse for {model_id}")
-
-        storage.quantity -= quantity
-        await warehouse.save()
-
-    async def __get_storage(self, model_id, size) -> Tuple[Warehouse, Storage]:
-        """Get size storage for model
-
-        Args:
-            model_id (_type_): model identifier
-            size (_type_): model size
-
-        Returns:
-            Tuple[Warehouse, Storage]: Model's Warehouse and size storage
+            Raises:
+            DBException:
+                Storage not found
+            ValueException:
+                Number of pairs in storage is less than quantity to take.
+                Size is not found
         """
         warehouse = await self.get_warehouse(model_id)
         if warehouse is None:
@@ -79,7 +57,10 @@ class WarehouseRepository:
         storage = next((s for s in warehouse.storage if s.size == size), None)
         if storage is None:
             raise ValueException(f"Size for {model_id} not found")
-        return warehouse, storage
+        storage.quantity += quantity
+        if storage.quantity < 0:
+            raise ValueException(f"Not enough pairs in warehouse for {model_id}")
+        await warehouse.save()
 
     async def get_warehouse(self, model_id: UUID):
         return await Warehouse.find(Warehouse.model_id == model_id).first_or_none()
