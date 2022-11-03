0e2ba06ec82d643af0606d0a1dd2b86dad1d1741
Darbestor <fordarbe@gmail.com>
2022-11-03 13:02:24 +0300

    initial setup

---
+++ order/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+
+from exceptions import DBException
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def initialize():
+    """Startup event"""
+    pass
+
+
+@app.exception_handler(ValueError)
+async def value_exception_handler(_: Request, exc: ValueError):
+    return JSONResponse(
+        status_code=422,
+        content={exc.args[0]: exc.args[1]},
+    )
+
+
+@app.exception_handler(DBException)
+async def db_exception_handler(_: Request, exc: DBException):
+    return JSONResponse(status_code=400, content={"database": exc.args[0]})
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(_: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=422,
+        content={ex["loc"][1]: ex["msg"] for ex in exc.errors()},
+    )
