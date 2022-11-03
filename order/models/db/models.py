fc6a18653f2fdf6eda1d46734d7fc33353386930
Darbestor <fordarbe@gmail.com>
2022-11-03 14:44:06 +0300

    repository

--- order/models/db/models.py
+++ order/models/db/models.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from uuid import UUID, uuid4
 from beanie import Document
 from pydantic import BaseModel, Field
+from pymongo import IndexModel
 
 
 class Item(BaseModel):
@@ -16,14 +17,17 @@ class Item(BaseModel):
 class Order(Document):
     """Mongodb model"""
 
+    id: UUID = Field(default_factory=uuid4)  # type: ignore
     user_id: UUID = Field(default_factory=uuid4)  # type: ignore
     items: list[Item]
-    address: str
-    phone: int
-    shipping_date: datetime
+    address: str | None
+    phone: int | None
+    shipping_date: datetime | None
+    total_price: float
+    created_date: datetime
 
     class Settings:
         """settings"""
 
         name = "orders"
-        indexes = ["user_id"]
+        indexes = [IndexModel("created_date", expireAfterSeconds="900")]
