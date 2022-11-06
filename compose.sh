1e8422ccb19656e4e6522b13441ad00b65b7a26d
Darbestor <fordarbe@gmail.com>
2022-11-06 16:43:00 +0300

    added condition in compose script

--- compose.sh
+++ compose.sh
@@ -1,12 +1,28 @@
 #!/bin/sh
 
+build=false
+
+# if --build flag passed to compose then we should
+# execute prebuild tasks for services
+for var in "$@"
+    do
+        if [ "$var" = "--build" ]
+        then
+            build=true
+            break
+        fi
+done
+
 rm -f .env
+
 for d in */ ; do
+    # gather config for all services
     cat $d.env >> .env 2>/dev/null
-    if [ $1 = "up" ]
-    then
+    
+    if [ "$build" = true ] ; then
+        echo "Executing pre_build_tasks in directory $d"
         ./${d}pre_build_tasks.sh
     fi
-
 done
+
 docker compose "$@"
